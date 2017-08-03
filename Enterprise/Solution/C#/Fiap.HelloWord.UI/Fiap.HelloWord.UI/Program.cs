a47b0df1bc7a252616e3dfc26d499a6409b8cd8a
logonrm <logonrm@fiap.com.br>
2017-08-02 22:43:29 -0300

    dsada

---
+++ Enterprise/Solution/C#/Fiap.HelloWord.UI/Fiap.HelloWord.UI/Program.cs
@@ -0,0 +1,31 @@
+﻿using Fiap.HelloWord.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.HelloWord.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Carro carro = new Model.Carro("UBA-1030");
+
+            carro.Placa = "UBA-1040";
+            carro.Portas = 4;
+
+            Console.WriteLine("O Carro placa " + carro.Placa +
+                " contem " + carro.Portas + " portas" );
+            Console.ReadLine();
+
+            Moto moto = new Moto("TTT-1010");
+            
+
+            Console.WriteLine("Moto - Bau: " + moto.Bau);
+            Console.ReadLine();
+        }
+    }
+}
