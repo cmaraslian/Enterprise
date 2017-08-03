669710f7846d329dd8dc3c9589252ef4c6e609d9
Carlos Maraslian <cmaraslian@hotmail.com>
2017-08-03 14:28:04 -0300

    Ajustes

---
+++ Solution/Fiap.HelloWord.UI/Fiap.HelloWord.UI/Model/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.HelloWord.UI.Model
+{
+    class Veiculo
+    {
+
+        private string _placa;
+
+        public string Placa
+        {
+            get { return _placa; }
+            set { _placa = value; }
+        }
+
+        //Construtor 
+        public Veiculo(string placa)
+        {
+            _placa = placa;
+        }
+
+    }
+}
