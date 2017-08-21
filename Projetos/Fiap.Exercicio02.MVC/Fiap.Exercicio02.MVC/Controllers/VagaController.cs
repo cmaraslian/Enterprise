1f21cd5e4d5756c65643516e372fc0a35217aa71
logonrm <logonrm@fiap.com.br>
2017-08-21 20:50:44 -0300

    Exercicio 02 adicionado, concluido, falta verificar o menu

---
+++ Projetos/Fiap.Exercicio02.MVC/Fiap.Exercicio02.MVC/Controllers/VagaController.cs
@@ -0,0 +1,51 @@
+﻿using Fiap.Exercicio02.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exercicio02.MVC.Controllers
+{
+    public class VagaController : Controller
+    {
+
+        private static List<Vaga> _banco = new List<Vaga>();
+
+
+        [HttpGet]
+        public ActionResult cadastrar()
+        {
+            PopulaComboNiveis();
+
+            return View();
+        }
+
+        private void PopulaComboNiveis()
+        {
+            List<string> niveis = new List<string>();
+            niveis.Add("Junior");
+            niveis.Add("Pleno");
+            niveis.Add("Senior");
+            ViewBag.opcoes = new SelectList(niveis);
+        }
+
+        //mvcaction4
+
+        [HttpPost]
+        public ActionResult cadastrar(Vaga vaga)
+        {
+            PopulaComboNiveis();
+            TempData["msg"] = "Cadastrado!";
+            _banco.Add(vaga);
+            return View();
+        }
+
+        public ActionResult Listar()
+
+        {
+            return View(_banco);
+        }
+
+    }
+}
---
+++ Projetos/Fiap.Exercicio02.MVC/Fiap.Exercicio02.MVC/Models/Vaga.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exercicio02.MVC.Models
+{
+    public class Vaga
+    {
+
+        public string Cargo { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+        [Display(Name = "Salário")]
+        public decimal Salario { get; set; }
+
+        [Display(Name = "Vale Refeição")]
+        public bool ValeRefeicao { get; set; }
+        [Display(Name = "Nível")] //Mostra nome usando acentos
+        public string Nivel { get; set; }
+
+        
+
+    }
+}
