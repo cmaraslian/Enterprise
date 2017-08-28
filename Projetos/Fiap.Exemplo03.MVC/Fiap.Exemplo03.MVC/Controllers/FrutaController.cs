2c3cd6d3fee5148ce7a3286596797ae60c9eca3c
logonrm <logonrm@fiap.com.br>
2017-08-28 20:52:40 -0300

    MVC Persistencia Introtucao

---
+++ Projetos/Fiap.Exemplo03.MVC/Fiap.Exemplo03.MVC/Controllers/FrutaController.cs
@@ -0,0 +1,33 @@
+﻿using Fiap.Exemplo03.MVC.Models;
+using Fiap.Exemplo03.MVC.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo03.MVC.Controllers
+{
+    public class FrutaController : Controller
+    {
+        private SacolaoContext _context = new SacolaoContext();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {  
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Fruta fruta)
+        {
+            fruta.DataCadastro = DateTime.Now;
+            _context.Frutas.Add(fruta);
+            _context.SaveChanges();
+            TempData["msg"] = "Cadastrado!";
+            //Para nao cadastrar novamente no F5
+            return RedirectToAction("Cadastrar");
+        }
+
+    }
+}
